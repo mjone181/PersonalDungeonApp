f0e89c9899e23fff65b1de137f27e29751272a3c
mjone181 <mjone181@gmail.com>
2020-12-14 12:37:46 -0600

    Add project files.

---
+++ DungeonLibrary/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public abstract class Character
+    {
+        //This is the main structure that both Player and Enemy characters will inherit from.
+
+        //Fields
+
+        //Used to manipuate HP status of current Player/Monster.
+        private int _health;
+
+        //Properties        
+        public string Name { get; set; }
+        public int Accuracy { get; set; }
+        public int Defense { get; set; }
+        public int MaxHP { get; set; }
+        
+        //CTORS
+
+        //Methods
+    }
+}
---
+++ PersonalDungeonApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DungeonLibrary;
+namespace PersonalDungeonApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Greet the User open application start.
+            Console.Title = "Giant Scary Cave";
+            Console.WriteLine("Welcome to the Giant Scary Cave");
+
+            //Running total of monsters killed.
+            int killCount = 0;
+        }
+    }
+}
